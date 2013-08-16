528682961790bd4a4317c23d0160fe1db0fcdafa
Scott Kingsley Clark <lol@scottkclark.com>
2013-08-16 12:50:53 -0500

    First commit to init repo with fork of @brichards 2checkout repo w/ PayPal Pro references changed and settings defined

---
+++ lib/addon-functions.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * The following file contains utility functions specific to our PayPal Pro add-on
+ * If you're building your own transaction-method addon, it's likely that you will
+ * need to do similar things. This includes enqueueing scripts, formatting data for PayPal Pro, etc.
+*/
+
+/**
+ * Adds actions to the plugins page for the iThemes Exchange PayPal Pro plugin
+ *
+ * @since 1.0.0
+ *
+ * @param array $meta Existing meta
+ * @param string $plugin_file the wp plugin slug (path)
+ * @param array $plugin_data the data WP harvested from the plugin header
+ * @param string $context
+ * @return array
+*/
+function it_exchange_paypal_pro_plugin_row_actions( $actions, $plugin_file, $plugin_data, $context ) {
+
+    $actions['setup_addon'] = '<a href="' . get_admin_url( NULL, 'admin.php?page=it-exchange-addons&add-on-settings=paypal-pro' ) . '">' . __( 'Setup Add-on', 'it-l10n-exchange-addon-paypal-pro' ) . '</a>';
+
+    return $actions;
+
+}
+add_filter( 'plugin_action_links_exchange-addon-paypal-pro/exchange-addon-paypal-pro.php', 'it_exchange_paypal_pro_plugin_row_actions', 10, 4 );
+
+/**
+ * Enqueues any scripts we need on the frontend during a PayPal Pro checkout
+ *
+ * @since 1.0.0
+ *
+ * @return void
+*/
+function it_exchange_paypal_pro_addon_enqueue_script() {
+    wp_enqueue_script( 'paypal-pro-addon-js', ITUtility::get_url_from_file( dirname( __FILE__ ) ) . '/js/paypal-pro-addon.js', array( 'jquery' ) );
+    wp_localize_script( 'paypal-pro-addon-js', 'PaypalProAddonL10n', array(
+            'processing_payment_text' => __( 'Processing payment, please wait...', 'it-l10n-exchange-addon-paypal-pro' ),
+        )
+    );
+}
+add_action( 'wp_enqueue_scripts', 'it_exchange_paypal_pro_addon_enqueue_script' );
+
+/**
+ * Grab the PayPal Pro customer ID for a WP user
+ *
+ * @since 1.0.0
+ *
+ * @param integer $customer_id the WP customer ID
+ * @return integer
+*/
+function it_exchange_paypal_pro_addon_get_customer_id( $customer_id ) {
+    $settings = it_exchange_get_option( 'addon_paypal_pro' );
+    $mode     = ( $settings['paypal-pro-test-mode'] ) ? '_test_mode' : '_live_mode';
+
+    return get_user_meta( $customer_id, '_it_exchange_paypal_pro_id' . $mode, true );
+}
+
+/**
+ * Add the PayPal Pro customer ID as user meta on a WP user
+ *
+ * @since 1.0.0
+ *
+ * @param integer $customer_id the WP user ID
+ * @param integer $paypal_pro_id the PayPal Pro customer ID
+ * @return boolean
+*/
+function it_exchange_paypal_pro_addon_set_customer_id( $customer_id, $paypal_pro_id ) {
+    $settings = it_exchange_get_option( 'addon_paypal_pro' );
+    $mode     = ( $settings['paypal-pro-test-mode'] ) ? '_test_mode' : '_live_mode';
+
+    return update_user_meta( $customer_id, '_it_exchange_paypal_pro_id' . $mode, $paypal_pro_id );
+}
+
+/**
+ * Grab a transaction from the PayPal Pro transaction ID
+ *
+ * @since 1.0.0
+ *
+ * @param integer $paypal_pro_id id of PayPal Pro transaction
+ * @return transaction object
+*/
+function it_exchange_paypal_pro_addon_get_transaction_id( $paypal_pro_id ) {
+    $args = array(
+        'meta_key'    => '_it_exchange_transaction_method_id',
+        'meta_value'  => $paypal_pro_id,
+        'numberposts' => 1, //we should only have one, so limit to 1
+    );
+    return it_exchange_get_transactions( $args );
+}
+
+/**
+ * Updates a PayPal Pro transaction status based on PayPal Pro ID
+ *
+ * @since 1.0.0
+ *
+ * @param integer $paypal_pro_id id of PayPal Pro transaction
+ * @param string $new_status new status
+ * @return void
+*/
+function it_exchange_paypal_pro_addon_update_transaction_status( $paypal_pro_id, $new_status ) {
+    $transactions = it_exchange_paypal_pro_addon_get_transaction_id( $paypal_pro_id );
+    foreach( $transactions as $transaction ) { //really only one
+        $current_status = it_exchange_get_transaction_status( $transaction );
+        if ( $new_status !== $current_status )
+            it_exchange_update_transaction_status( $transaction, $new_status );
+    }
+}
+
+/**
+ * Adds a refund to post_meta for a PayPal Pro transaction
+ *
+ * @since 1.0.0
+*/
+function it_exchange_paypal_pro_addon_add_refund_to_transaction( $paypal_pro_id, $refund ) {
+
+    // PayPal Pro money format comes in as cents. Divide by 100.
+    $refund = ( $refund / 100 );
+
+    // Grab transaction
+    $transactions = it_exchange_paypal_pro_addon_get_transaction_id( $paypal_pro_id );
+    foreach( $transactions as $transaction ) { //really only one
+
+        $refunds = it_exchange_get_transaction_refunds( $transaction );
+
+        $refunded_amount = 0;
+        foreach( ( array) $refunds as $refund_meta ) {
+            $refunded_amount += $refund_meta['amount'];
+        }
+
+        // In PayPal Pro the Refund is the total amount that has been refunded, not just this transaction
+        $this_refund = $refund - $refunded_amount;
+
+        // This refund is already formated on the way in. Don't reformat.
+        it_exchange_add_refund_to_transaction( $transaction, $this_refund );
+    }
+}
+
+/**
+ * Removes a PayPal Pro Customer ID from a WP user
+ *
+ * @since 1.0.0
+ *
+ * @param integer $paypal_pro_id the id of the PayPal Pro transaction
+*/
+function it_exchange_paypal_pro_addon_delete_id_from_customer( $paypal_pro_id ) {
+    $settings = it_exchange_get_option( 'addon_paypal_pro' );
+    $mode     = ( $settings['paypal-pro-test-mode'] ) ? '_test_mode' : '_live_mode';
+
+    $transactions = it_exchange_paypal_pro_addon_get_transaction_id( $paypal_pro_id );
+    foreach( $transactions as $transaction ) { //really only one
+        $customer_id = get_post_meta( $transaction->ID, '_it_exchange_customer_id', true );
+        if ( false !== $current_paypal_pro_id = it_exchange_paypal_pro_addon_get_customer_id( $customer_id ) ) {
+
+            if ( $current_paypal_pro_id === $paypal_pro_id )
+                delete_user_meta( $customer_id, '_it_exchange_paypal_pro_id' . $mode );
+
+        }
+    }
+}
---
+++ lib/required-hooks.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * Exchange Transaction Add-ons require several hooks in order to work properly.
+ * Most of these hooks are called in api/transactions.php and are named dynamically
+ * so that individual add-ons can target them. eg: it_exchange_refund_url_for_paypal_pro
+ * We've placed them all in one file to help add-on devs identify them more easily
+*/
+
+/**
+ * PayPal Pro URL to perform refunds
+ *
+ * The it_exchange_refund_url_for_[addon-slug] filter is
+ * used to generate the link for the 'Refund Transaction' button
+ * found in the admin under Customer Payments
+ *
+ * @since 1.0.0
+ *
+ * @param string $url passed by WP filter.
+ * @param string $url transaction URL
+*/
+function it_exchange_refund_url_for_paypal_pro( $url ) {
+	return 'https://www.2checkout.com/va/sales/';
+}
+add_filter( 'it_exchange_refund_url_for_paypal_pro', 'it_exchange_refund_url_for_paypal_pro' );
+
+/**
+ * This proccesses a PayPal Pro transaction.
+ *
+ * The it_exchange_do_transaction_[addon-slug] action is called when
+ * the site visitor clicks a specific add-ons 'purchase' button. It is
+ * passed the default status of false along with the transaction object
+ * The transaction object is a package of data describing what was in the user's cart
+ *
+ * Exchange expects your add-on to either return false if the transaction failed or to
+ * call it_exchange_add_transaction() and return the transaction ID
+ *
+ * @since 1.0.0
+ *
+ * @param string $status passed by WP filter.
+ * @param object $transaction_object The transaction object
+*/
+function it_exchange_paypal_pro_addon_process_transaction( $status, $transaction_object ) {
+
+	// If this has been modified as true already, return.
+	if ( $status )
+		return $status;
+
+	// Verify nonce
+	if ( ! empty( $_REQUEST['_paypal_pro_nonce'] ) && ! wp_verify_nonce( $_REQUEST['_paypal_pro_nonce'], 'paypal-pro-checkout' ) ) {
+		it_exchange_add_message( 'error', __( 'Transaction Failed, unable to verify security token.', 'it-l10n-exchange-addon-paypal-pro' ) );
+		return false;
+	}
+
+	// Make sure we have the correct $_POST argument
+	if ( ! empty( $_POST['PayPalProToken'] ) ) {
+
+		try {
+
+			$general_settings = it_exchange_get_option( 'settings_general' );
+			$settings         = it_exchange_get_option( 'addon_paypal_pro' );
+
+			// Get our secret key
+			$secret_key = ( $settings['paypal-pro-test-mode'] ) ? $settings['paypal-pro-test-secret-key'] : $settings['paypal-pro-live-secret-key'];
+
+			// Set PayPal Pro customer from WP customer ID
+			$it_exchange_customer = it_exchange_get_current_customer();
+
+			/*if ( $paypal_pro_id = it_exchange_paypal_pro_addon_get_customer_id( $it_exchange_customer->id ) )
+			 	$it_exchange_customer = Twocheckout_Customer::retrieve( $paypal_pro_id );*/
+
+			// Now that we have a valid Customer ID, charge them!
+			$charge = array(
+				'customer'    => $it_exchange_customer->id,
+				'amount'      => number_format( $transaction_object->total, 2, '', '' ),
+				'currency'    => $general_settings['default-currency'],
+				'description' => $transaction_object->description,
+			);
+
+		}
+		catch ( Exception $e ) {
+			it_exchange_add_message( 'error', $e->getMessage() );
+			return false;
+		}
+		return it_exchange_add_transaction( 'paypal-pro', $charge->id, 'succeeded', $it_exchange_customer->id, $transaction_object );
+	} else {
+		it_exchange_add_message( 'error', __( 'Unknown error. Please try again later.', 'it-l10n-exchange-addon-paypal-pro' ) );
+	}
+	return false;
+
+}
+add_action( 'it_exchange_do_transaction_paypal_pro', 'it_exchange_paypal_pro_addon_process_transaction', 10, 2 );
+
+/**
+ * Returns the button for making the payment
+ *
+ * Exchange will loop through activated Payment Methods on the checkout page
+ * and ask each transaction method to return a button using the following filter:
+ * - it_exchange_get_[addon-slug]_make_payment_button
+ * Transaction Method add-ons must return a button hooked to this filter if they
+ * want people to be able to make purchases.
+ *
+ * @since 1.0.0
+ *
+ * @param array $options
+ * @return string HTML button
+*/
+function it_exchange_paypal_pro_addon_make_payment_button( $options ) {
+
+    if ( 0 >= it_exchange_get_cart_total( false ) )
+        return;
+
+    $general_settings = it_exchange_get_option( 'settings_general' );
+    $paypal_pro_settings = it_exchange_get_option( 'addon_paypal_pro' );
+
+    $publishable_key = ( $paypal_pro_settings['paypal-pro-test-mode'] ) ? $paypal_pro_settings['paypal-pro-test-publishable-key'] : $paypal_pro_settings['paypal-pro-live-publishable-key'];
+
+    $products = it_exchange_get_cart_data( 'products' );
+
+    $payment_form = '<form class="paypal-pro_form" action="' . esc_attr( it_exchange_get_page_url( 'transaction' ) ) . '" method="post">';
+    $payment_form .= '<input type="hidden" name="it-exchange-transaction-method" value="paypal-pro" />';
+    $payment_form .= wp_nonce_field( 'paypal-pro-checkout', '_paypal_pro_nonce', true, false );
+
+    $payment_form .= '<div class="hide-if-no-js">';
+    $payment_form .= '<input type="submit" class="it-exchange-paypal-pro-payment-button" name="paypal-pro_purchase" value="' . esc_attr( $paypal_pro_settings['paypal-pro-purchase-button-label'] ) .'" />';
+
+    $payment_form .= '<script>' . "\n";
+    $payment_form .= '  jQuery(".it-exchange-paypal-pro-payment-button").click(function(){' . "\n";
+    $payment_form .= '    var token = function(res){' . "\n";
+    $payment_form .= '      var $paypal_proToken = jQuery("<input type=hidden name=PayPalProToken />").val(res.id);' . "\n";
+    $payment_form .= '      jQuery("form.paypal-pro_form").append($paypal_proToken).submit();' . "\n";
+    $payment_form .= '      it_exchange_paypal_pro_processing_payment_popup();' . "\n";
+    $payment_form .= '    };' . "\n";
+    $payment_form .= '    PayPal ProCheckout.open({' . "\n";
+    $payment_form .= '      key:         "' . esc_js( $publishable_key ) . '",' . "\n";
+    $payment_form .= '      amount:      "' . esc_js( number_format( it_exchange_get_cart_total( false ), 2, '', '' ) ) . '",' . "\n";
+    $payment_form .= '      currency:    "' . esc_js( $general_settings['default-currency'] ) . '",' . "\n";
+    $payment_form .= '      name:        "' . empty( $general_settings['company-name'] ) ? '' : esc_js( $general_settings['company-name'] ) . '",' . "\n";
+    $payment_form .= '      description: "' . esc_js( it_exchange_get_cart_description() ) . '",' . "\n";
+    $payment_form .= '      panelLabel:  "Checkout",' . "\n";
+    $payment_form .= '      token:       token' . "\n";
+    $payment_form .= '    });' . "\n";
+    $payment_form .= '    return false;' . "\n";
+    $payment_form .= '  });' . "\n";
+    $payment_form .= '</script>' . "\n";
+
+    $payment_form .= '</form>';
+    $payment_form .= '</div>';
+
+    return $payment_form;
+}
+add_filter( 'it_exchange_get_paypal_pro_make_payment_button', 'it_exchange_paypal_pro_addon_make_payment_button', 10, 2 );
+
+/**
+ * Gets the interpretted transaction status from valid PayPal Pro transaction statuses
+ *
+ * Most gateway transaction stati are going to be lowercase, one word strings.
+ * Hooking a function to the it_exchange_transaction_status_label_[addon-slug] filter
+ * will allow add-ons to return the human readable label for a given transaction status.
+ *
+ * @since 1.0.0
+ *
+ * @param string $status the string of the PayPal Pro transaction
+ * @return string translaction transaction status
+*/
+function it_exchange_paypal_pro_addon_transaction_status_label( $status ) {
+    switch ( $status ) {
+        case 'succeeded':
+            return __( 'Paid', 'it-l10n-exchange-addon-paypal-pro' );
+        case 'refunded':
+            return __( 'Refunded', 'it-l10n-exchange-addon-paypal-pro' );
+        case 'partial-refund':
+            return __( 'Partially Refunded', 'it-l10n-exchange-addon-paypal-pro' );
+        case 'needs_response':
+            return __( 'Disputed: PayPal Pro needs a response', 'it-l10n-exchange-addon-paypal-pro' );
+        case 'under_review':
+            return __( 'Disputed: Under review', 'it-l10n-exchange-addon-paypal-pro' );
+        case 'won':
+            return __( 'Disputed: Won, Paid', 'it-l10n-exchange-addon-paypal-pro' );
+        default:
+            return __( 'Unknown', 'it-l10n-exchange-addon-paypal-pro' );
+    }
+}
+add_filter( 'it_exchange_transaction_status_label_paypal_pro', 'it_exchange_paypal_pro_addon_transaction_status_label' );
+
+/**
+ * Returns a boolean. Is this transaction a status that warrants delivery of any products attached to it?
+ *
+ * Just because a transaction gets added to the DB doesn't mean that the admin is ready to give over
+ * the goods yet. Each payment gateway will have different transaction stati. Exchange uses the following
+ * filter to ask transaction-methods if a current status is cleared for delivery. Return true if the status
+ * means its okay to give the download link out, ship the product, etc. Return false if we need to wait.
+ * - it_exchange_[addon-slug]_transaction_is_cleared_for_delivery
+ *
+ * @since 1.0.0
+ *
+ * @param boolean $cleared passed in through WP filter. Ignored here.
+ * @param object $transaction
+ * @return boolean
+*/
+function it_exchange_paypal_pro_transaction_is_cleared_for_delivery( $cleared, $transaction ) {
+    $valid_stati = array( 'succeeded', 'partial-refund', 'won' );
+    return in_array( it_exchange_get_transaction_status( $transaction ), $valid_stati );
+}
+add_filter( 'it_exchange_paypal_pro_transaction_is_cleared_for_delivery', 'it_exchange_paypal_pro_transaction_is_cleared_for_delivery', 10, 2 );
